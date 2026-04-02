61d63f6cec491c8cefc8324885b92bc8b95bf220
Tan Nguyen <nguyennhattan2707@gmail.com>
2026-04-02 15:34:44 -0700

    Update for IA1

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Table from "./Table";
+
+const characters = [
+  {
+    name: "Charlie",
+    job: "Janitor"
+  },
+  {
+    name: "Mac",
+    job: "Bouncer"
+  },
+  {
+    name: "Dee",
+    job: "Aspring actress"
+  },
+  {
+    name: "Dennis",
+    job: "Bartender"
+  }
+];
+
+function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor" // the rest of the data
+    }
+  ])
+  return (
+    <div className="container">
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
+    </div>
+);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+}
+
+export default MyApp;
